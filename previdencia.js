f135c81b1d46bf5f2dfbf78f7f7ab561cfad20ba
Miguel P. Granville <85964058+forti8@users.noreply.github.com>
2023-08-19 22:01:26 -0300

    Update previdencia.js

--- previdencia.js
+++ previdencia.js
@@ -1,33 +1,38 @@
-
 class Previdencia {
-constructor () {
-this.users=[];
-this.prevd=[];
-this.juros;
-};
+  constructor() {
 
-/*
-esta função esta atualmente em desinvolvimento
-logo sera compilada uma nova versão
-*/
+    this.users = [];
+    this.prevd = [];
+    this.juros = 10;
+  }
+
+  comprarPrev(id, nomePrevidencia) {
+  const userVer = this.users.find(user => user.id === id);
+  const prevdV = this.prevd.find(previdencia => previdencia[nomePrevidencia]);
   
-comprarPrev(user, num) {
-const userVer = this.users.find(c => c.id === user);
-const prevVer = this.prevd[`${num}`]
-if (userVer && prevVer) {
-     console.log("test") 
-var l = {prevVer}
-        this.users[user].push(l)
+  if (userVer && prevdV) {
+    const previdenciaInfo = prevdV[nomePrevidencia];
+    const novaPrevidencia = {
+      nome: previdenciaInfo.nome,
+      valor: previdenciaInfo.valor
+    };
+    
+    userVer.previdencias = userVer.previdencias || [];
+    userVer.previdencias.push(novaPrevidencia);
+  }
 }
+
+adicionarPrevList(p) {
+this.prevd.push(p)
 }
-registrarUsuarios(user, j, p) {
-        this.prevd.push(p);
-    for (const obj of user) {
+
+  registrarUsuarios(users) {
+    for (const obj of users) {
       if (obj.nome) {
         this.users.push(obj);
       }
     }
-}
+  }
 }
 
-module.exports=Previdencia;
+module.exports = Previdencia;
